e9ce68213417491954634617d33a78466aaf9daa
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 15:51:54 +0100

    Enable the HTML transform in `eleventy.config.js`

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,22 @@
 import baseline, { config as baselineConfig } from "@apleasantview/eleventy-plugin-baseline";
 
+import path from "node:path";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(baseline({
-    assetsESBuild: {
-      minify: process.env.ELEVENTY_ENV === "production",
-      target: "es2017",
+  eleventyConfig.addPlugin(baseline());
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    urlPath: "/media/",
+    outputDir: "./dist/media/",
+    formats: ["avif", "webp", "jpeg"],
+    widths: [320, 640, 960, 1280],
+    filenameFormat(id, src, width, format) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+      return `${name}-${width}w.${format}`;
     },
-  }));
+  });
 }
 
 export const config = baselineConfig;
