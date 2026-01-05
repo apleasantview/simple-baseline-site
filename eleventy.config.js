6539a7f76e6868e9e4bdf07bfa77592e392c2500
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 12:04:19 +0100

    Enable (or skip) the navigator template

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,9 @@ import baseline, { config as baselineConfig } from "@apleasantview/eleventy-plug
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(baseline());
+  eleventyConfig.addPlugin(baseline({
+    enableNavigatorTemplate: true, // local dev only; disable for production
+  }));
 }
 
 export const config = baselineConfig;
