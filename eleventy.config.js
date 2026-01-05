602b7a4eacca212643ed609014eb3696fc48e59f
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 11:15:32 +0100

    Ensure Baseline is loaded

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,6 @@ import baseline, { config as baselineConfig } from "@apleasantview/eleventy-plug
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
-  // Defaults enable assets-core, assets-postcss, assets-esbuild.
   eleventyConfig.addPlugin(baseline());
 }
 
