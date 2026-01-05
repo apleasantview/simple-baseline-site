0abab34345b38158b96725da1690dbda08b173b3
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 10:03:17 +0100

    Required site data

---
+++ src/_data/head.js
@@ -0,0 +1,4 @@
+export default {
+  link: [{ rel: "stylesheet", href: "/assets/css/index.css" }],
+  script: [{ src: "/assets/js/index.js", defer: true }]
+};
---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+export default {
+  title: "Simple Baseline Site",
+  tagline: "Hello, Eleventy + Baseline",
+  url: process.env.URL || "http://localhost:8080/",
+  defaultLanguage: "en",
+  noindex: false
+};
