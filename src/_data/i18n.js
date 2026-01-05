f66b16dabe51f1672344a2906c6151f92bc964c4
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 10:20:46 +0100

    Define languages

---
+++ src/_data/i18n.js
@@ -0,0 +1,19 @@
+export default {
+  defaultLanguage: "en",
+  languages: {
+    en: {
+      contentDir: "content/en/",
+      languageCode: "en",
+      languageName: "English",
+      title: "Baseline (EN)",
+      tagline: "Hello"
+    },
+    nl: {
+      contentDir: "content/nl/",
+      languageCode: "nl",
+      languageName: "Nederlands",
+      title: "Baseline (NL)",
+      tagline: "Hallo"
+    }
+  }
+};
