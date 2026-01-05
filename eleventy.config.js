88222c5d115d7a3ade49342587bcf64f041c04a5
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 12:36:14 +0100

    Ensure Baseline is loaded

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,6 @@ import baseline, { config as baselineConfig } from "@apleasantview/eleventy-plug
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
-  // Sitemap is enabled by default
   eleventyConfig.addPlugin(baseline());
 }
 
