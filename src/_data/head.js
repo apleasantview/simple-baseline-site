da4cb16c7d055573a6f73aa06aa11d01f2954252
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 12:37:49 +0100

    Set site-wide social defaults

--- src/_data/head.js
+++ src/_data/head.js
@@ -1,4 +1,18 @@
 export default {
   link: [{ rel: "stylesheet", href: "/assets/css/index.css" }],
-  script: [{ src: "/assets/js/index.js", defer: true }]
+  script: [{ src: "/assets/js/index.js", defer: true }],
+  openGraph: {
+    "og:title": "Baseline Starter",
+    "og:description": "An Eleventy site using Baseline",
+    "og:type": "website",
+    "og:image": "https://example.com/assets/social/default-og.jpg"
+  },
+  twitter: {
+    "twitter:card": "summary_large_image",
+    "twitter:title": "Baseline Starter",
+    "twitter:description": "An Eleventy site using Baseline",
+    "twitter:image": "",
+    "twitter:site": "@yoursite",
+    "twitter:creator": "@yoursite"
+  },
 };
