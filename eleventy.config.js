656c313efd149ad31f67167c4319ad7c07be378a
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-05 15:28:58 +0100

    Set default target/minify for all bundles

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,12 @@ import baseline, { config as baselineConfig } from "@apleasantview/eleventy-plug
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(baseline());
+  eleventyConfig.addPlugin(baseline({
+    assetsESBuild: {
+      minify: process.env.ELEVENTY_ENV === "production",
+      target: "es2017",
+    },
+  }));
 }
 
 export const config = baselineConfig;
